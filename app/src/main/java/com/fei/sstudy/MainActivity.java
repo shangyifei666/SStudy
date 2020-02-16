2568493a076b63afab1d8e548dcd25a7c1b907f4
shangyifei <shangyifei@dding.cn>
2020-02-16 17:38:25 +0800

    add:底部栏UI与主题颜色

--- app/src/main/java/com/fei/sstudy/MainActivity.java
+++ app/src/main/java/com/fei/sstudy/MainActivity.java
@@ -2,7 +2,10 @@ package com.fei.sstudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.fei.sstudy.ui.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
