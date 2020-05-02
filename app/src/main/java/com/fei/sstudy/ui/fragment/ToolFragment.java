40464f6a610866c2407be4b3ae5929b95c09f83d
shangyifei <shangyifei@dding.cn>
2020-05-02 15:09:26 +0800

    add:工具栏

--- app/src/main/java/com/fei/sstudy/ui/fragment/ToolFragment.java
+++ app/src/main/java/com/fei/sstudy/ui/fragment/ToolFragment.java
@@ -1,6 +1,7 @@
 package com.fei.sstudy.ui.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.fei.sstudy.R;
+import com.fei.sstudy.ui.CheckPlaceActivity;
+import com.fei.sstudy.util.StatusBarUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ToolFragment extends Fragment {
+public class ToolFragment extends Fragment implements View.OnClickListener {
 
+    private ImageView ivZixi;
+    private TextView tvZixi;
 
     public ToolFragment() {
         // Required empty public constructor
@@ -26,7 +33,42 @@ public class ToolFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tool, container, false);
+        View view = inflater.inflate(R.layout.fragment_tool, container, false);
+        changeStatusBar();
+
+        initView(view);
+
+        return view;
     }
 
+    private void initView(View view){
+        ivZixi = view.findViewById(R.id.iv_zixi);
+        tvZixi = view.findViewById(R.id.tv_zixi);
+        ivZixi.setOnClickListener(this);
+        tvZixi.setOnClickListener(this);
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        changeStatusBar();
+    }
+
+    private void changeStatusBar(){
+        StatusBarUtils.setWindowStatusBarColor(getActivity(),getResources().getColor(R.color.color_tool_system));
+        StatusBarUtils.setWindowTextDayBarColor(getActivity());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_zixi:
+            case R.id.iv_zixi:
+                Intent intent = new Intent(getContext(), CheckPlaceActivity.class);
+                intent.putExtra("id",1);
+                intent.putExtra("h",1);
+                startActivityForResult(intent,0);
+                break;
+        }
+    }
 }
